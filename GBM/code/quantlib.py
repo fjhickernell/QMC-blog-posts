995f58a6d58d3de7db2810bfc7b31e99c52b14ee
Fred Hickernell <hickernell@illinoistech.edu>
2025-09-01 02:34:31 -0500

    Move Python file to the gbm folder

--- GBM/code/quantlib.py
+++ GBM/code/quantlib.py
@@ -15,7 +15,7 @@ def generate_quantlib_paths(initial_value, mu, sigma, maturity, n_steps, n_paths
     return paths, generation_time
 
 # Parameters
-params = {'initial_value': 100, 'mu': 0.05, 'sigma': 0.2, 'maturity': 1.0, 'n_steps': 252, 'n_paths': 2**14}
+params_ql = {'initial_value': 100, 'mu': 0.05, 'sigma': 0.2, 'maturity': 1.0, 'n_steps': 252, 'n_paths': 2**14}
 
 # Generate paths 
-quantlib_paths, quantlib_time = generate_quantlib_paths(**params)
+quantlib_paths, quantlib_time = generate_quantlib_paths(**params_ql)
