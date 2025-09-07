74ba6710655a8bcbc9d02b2071f496db93986fa9
Fred Hickernell <hickernell@illinoistech.edu>
2025-09-07 07:05:21 -0500

    update gbm code

--- GBM/code/gbm_lattice_32.py
+++ GBM/code/gbm_lattice_32.py
@@ -1,2 +1,2 @@
-gbm_lattice = plot_paths('GBM', qp.Lattice(2**8), t_final=5, 
+gbm_lattice = plot_paths('GBM', qp.Lattice(2**8, seed=42), t_final=5, 
                           initial_value=50, drift=0.1, diffusion=0.2, n=32)
